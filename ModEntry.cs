58c895d632aed92fde718135e14e7e9a32571803
Hawker <kovujacksonbentley@gmail.com>
2025-03-02 10:55:45 -0500

    Initial Commit

---
+++ ModEntry.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace Instant_Community_Center_Cheat
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.GameLoop.UpdateTicking += this.UpdateTicking;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+        }
+
+        private void UpdateTicking(object? sender, UpdateTickingEventArgs e)
+        {
+            Log("Instant_Community_Center_Cheat");
+        }
+
+        private void Log(string message, LogLevel level = LogLevel.Debug)
+        {
+            this.Monitor.Log(message, level);
+        }
+    }
+}
