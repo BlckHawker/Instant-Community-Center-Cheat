b3a43399e1e97b06bbb8903c77c4e3d1b6cd25e7
Hawker <kovujacksonbentley@gmail.com>
2025-04-03 10:53:26 -0400

    Add Joja route

--- ModConfig.cs
+++ ModConfig.cs
@@ -15,7 +15,13 @@ namespace Instant_Community_Center_Cheat
         /// <summary>The default key for giving a player an item</summary>
         public const SButton DefaultGiveCCItemKey = SButton.L;
 
+        /// <summary>The default bool value of if the player wants to complete the CC through Joja</summary>
+        public const bool DefaultJoja = false;
+
         /// <summary>The key which gives the player the desired itemss.</summary>
         public SButton GiveCCItemKey { get; set; } = DefaultGiveCCItemKey;
+
+        ///<summary>If the player wants to complete the CC through Joja</summary>
+        public bool Joja { get; set; } = DefaultJoja;
     }
 }
